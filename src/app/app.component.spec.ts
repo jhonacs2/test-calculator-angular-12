702d356d8578148743691eedee3c556bf58e2400
Jhonatan Soto <jhonrocker2012@gmail.com>
2024-09-13 22:05:01 -0400

    testÃÃ: add unit tests for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,18 @@
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
+  let appComponentRef: ComponentFixture<AppComponent>;
+  let appComponentElement: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
+
+    appComponentRef = TestBed.createComponent(AppComponent);
+    appComponentElement = appComponentRef.nativeElement;
+    appComponentRef.detectChanges()
   });
 
   it('should create the app', () => {
@@ -16,9 +21,29 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularCalculator12'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  it(`should have the calculator title'`, () => {
+    const app = appComponentRef.componentInstance;
     expect(app.title).toEqual('angular-calculator');
   });
+
+  it(`should render router-outlet'`, () => {
+    expect(appComponentElement.querySelector('router-outlet')).not.toBeNull();
+  })
+
+  it('should render router-outlet wrapped with css classes', () => {
+    const routerElementParent = appComponentElement.querySelector('div');
+    const cssClasses = 'min-w-screen min-h-screen bg-slate-500 flex items-center justify-center px-5 py-5'.split(" ");
+    const divClasses = routerElementParent?.classList.value.split(' ');
+
+    cssClasses.forEach(className => expect(divClasses).toContain(className));
+  });
+
+  it(`should contain the buy me a beer link`, () => {
+    const linkElement = appComponentElement.querySelector('a');
+
+    expect(linkElement).not.toBeNull();
+    expect(linkElement?.title).toBeTruthy();
+    expect(linkElement?.title).toBe('Buy me a beer');
+    expect(linkElement?.href).toBeTruthy();
+  })
 });
